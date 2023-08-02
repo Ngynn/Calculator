ba9acbfa895536c20b2c66c4e82564baa6fa23a0
Long <ikon0709@gmail.com>
2023-08-02 19:37:41 +0700

    none agrx

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
